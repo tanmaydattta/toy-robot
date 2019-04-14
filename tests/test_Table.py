e4ec21b5b7e640fb9ce83acef3dcfeff1a27da71
Tanmay Datta <tanmay.datta86@gmail.com>
2019-04-14 10:43:29 +0800

    Table tests

--- tests/test_Table.py
+++ tests/test_Table.py
@@ -24,3 +24,9 @@ class TableTest(unittest.TestCase):
         table = Table(length, width)
         self.assertEqual(table.width, width)
         self.assertEqual(table.length, length)
+
+    @ddt.data((-4, 4), (6, -6), (-9, -9))
+    @ddt.unpack
+    def test_if_table_throws_valueerror(self, length, width):
+        with self.assertRaises(ValueError):
+            Table(length, width)
