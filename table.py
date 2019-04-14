0ed091df90cb20c6014aa48be233b826ff587d4f
Tanmay Datta <tanmay.datta86@gmail.com>
2019-04-14 10:43:48 +0800

    table class is done

---
+++ table.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Main module for Table class
+
+"""
+import logging
+from os import path
+import logging.config
+
+__author__ = "tanmay.datta86@gmail.com"
+logging.config.fileConfig('logging.config')
+logger = logging.getLogger('toy_robot')
+
+
+class Table:
+    """
+    Table on which robot will be placed
+    """
+
+    def __init__(self, length, width):
+        if (length < 1 or width < 1):
+            raise ValueError("Table length and width should be more than 0")
+        logger.debug("Table init with length: {} width {}".format(length,
+                                                                  width))
+        self.length = length
+        self.width = width
+
+    def is_valid_position(self, x_pos: int, y_pos: int) -> bool:
+        """
+        Given x and y coordinates return if the location is on table
+            :param self: insance
+            :param x_pos: x co-ordinate
+            :param y_pos: y co-ordinate
+        """
+        logger.debug("Table with length: {} width {}".format(self.length,
+                                                             self.width))
+        logger.debug(
+            "Position with co-ordinates: x {} y {}".format(x_pos, y_pos))
+        return (0 <= x_pos <= self.width) and (0 <= y_pos <= self.length)
+
+    def __str__(self):
+        return "Width: {width}, Length: {length}".format(width=self.width,
+                                                         length=self.length)
