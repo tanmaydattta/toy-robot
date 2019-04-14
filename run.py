d8b450fd946e55215d04aed536f1843bc8c5fede
Tanmay Datta <tanmay.datta86@gmail.com>
2019-04-14 19:03:47 +0800

    run.py commands updated report works place works

--- run.py
+++ run.py
@@ -7,6 +7,7 @@ from cmd import Cmd
 import logging.config
 
 from robot import Robot
+from table import Table
 
 __author__ = "tanmay.datta86@gmail.com"
 logging.config.fileConfig('logging.config',
@@ -18,8 +19,9 @@ class RobotPrompt(Cmd):
     To quit you can just type q and enter or just press Ctrl+C
     Type help or ? to list commands.\n"""
     prompt = '(Toy robot) '
+    robot = Robot(Table(5,5))
 
-    def do_place(self, x_coord, y_coord, face):
+    def do_place(self, place_cmd):
         """
         Places a robot on the the defaul 5X5 table.
         Position (x,y) and facing "F"
@@ -27,7 +29,12 @@ class RobotPrompt(Cmd):
         Syntax is place[PLACE] x,y,f(acing)
         example place 4,4,EAST => place at 4,4 facing EAST
         """
-        print("okay this works nicely {}".format(x_coord))
+        x_coord, y_coord, direction = place_cmd.split(",")
+        x_coord = int(x_coord)
+        y_coord = int(y_coord)
+        direction = direction.upper()
+        self.robot.place(x_coord, y_coord, direction)
+        print("okay this works nicely {}".format(direction))
 
     def do_move(self):
         """
@@ -51,7 +58,7 @@ class RobotPrompt(Cmd):
         """
         pass
 
-    def do_right(self):
+    def do_right(self, _line):
         """
         Moves robot to it's right direction
         ----->
@@ -63,12 +70,13 @@ class RobotPrompt(Cmd):
         """
         pass
 
-    def do_report(self):
+    def do_report(self, _line):
         """
         Prints the location of robot
         Prints nothing if robot is not "placed" yet
         """
-        pass
+        print(_line)
+        print(self.robot.report())
 
     def precmd(self, line):
             line = line.lower()
@@ -80,5 +88,4 @@ class RobotPrompt(Cmd):
 
 if __name__ == "__main__":
     prompt = RobotPrompt()
-    prompt.robot = Robot()
     prompt.cmdloop()
