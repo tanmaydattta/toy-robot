208e6a820a95a8d7e6f59e23d886bf343a7db315
Tanmay Datta <tanmay.datta86@gmail.com>
2019-04-14 10:49:32 +0800

    full table tests

--- tests/test_Table.py
+++ tests/test_Table.py
@@ -14,7 +14,7 @@ __author__ = "tanmay.datta86@gmail.com"
 @ddt.ddt
 class TableTest(unittest.TestCase):
 
-    def setup(self):
+    def setUp(self):
         self.default_table = Table(5, 5)
 
     @ddt.data((4, 4), (6, 6), (9, 9))
@@ -30,3 +30,13 @@ class TableTest(unittest.TestCase):
     def test_if_table_throws_valueerror(self, length, width):
         with self.assertRaises(ValueError):
             Table(length, width)
+
+    @ddt.data((4, 4), (3, 3), (5, 4), (0, 0))
+    @ddt.unpack
+    def test_if_given_cordinates_are_on_table(self, x_pos, y_pos):
+        self.assertTrue(self.default_table.is_valid_position(x_pos, y_pos))
+
+    @ddt.data((9, 9), (9, 3), (5, 7), (-7, 0))
+    @ddt.unpack
+    def test_if_given_cordinates_are_not_on_table(self, x_pos, y_pos):
+        self.assertFalse(self.default_table.is_valid_position(x_pos, y_pos))
